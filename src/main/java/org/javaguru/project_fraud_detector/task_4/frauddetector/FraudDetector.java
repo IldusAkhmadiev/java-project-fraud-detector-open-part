d939cd8569952a66ef1ad64fcd833150976326f3
Ildus Akhmadiev <anan308an@gmail.com>
2024-08-19 16:59:56 +0300

    Update isFraud method to detect transactions over 1 million and add new unit tests

--- src/main/java/org/javaguru/project_fraud_detector/task_4/frauddetector/FraudDetector.java
+++ src/main/java/org/javaguru/project_fraud_detector/task_4/frauddetector/FraudDetector.java
@@ -3,8 +3,11 @@ package org.javaguru.project_fraud_detector.task_4.frauddetector;
 class FraudDetector {
 
     boolean isFraud(Transaction transaction) {
+        if(transaction.getAmount() > 1000000) {
+            return true;
+        }
         Trader trader = transaction.getTrader();
-        if (trader.getFullName().equals("Pokemon")) {
+        if (trader.getFullName().equalsIgnoreCase("Pokemon")) {
             return true;
         } else {
             return false;
--- src/test/java/org/javaguru/project_fraud_detector/task_4/frauddetector/FraudDetectorTest.java
+++ src/test/java/org/javaguru/project_fraud_detector/task_4/frauddetector/FraudDetectorTest.java
@@ -22,5 +22,23 @@ class FraudDetectorTest {
         Transaction transaction = new Transaction(trader, 1000);
         assertFalse(fraudDetector.isFraud(transaction));
     }
+    @Test
+    public void isFraudAmountMoreThen1m() {
+        Trader trader = new Trader("Pokemon", "Moscow");
+        Trader trader3 = new Trader("Maks", "Moscow");
+        Transaction transaction = new Transaction(trader, 5000);
+        Transaction transaction3 = new Transaction(trader3, 5_000_000);
+        assertTrue(fraudDetector.isFraud(transaction));
+        assertTrue(fraudDetector.isFraud(transaction3));
+    }
 
+    @Test
+    public void isNotFraudAmountLessThen1m() {
+        Trader trader = new Trader("Rerix", "Moscow");
+        Trader trader2 = new Trader("Krio", "Moscow");
+        Transaction transaction = new Transaction(trader, 5000);
+        Transaction transaction2 = new Transaction(trader2, 999_999);
+        assertFalse(fraudDetector.isFraud(transaction));
+        assertFalse(fraudDetector.isFraud(transaction2));
+    }
 }

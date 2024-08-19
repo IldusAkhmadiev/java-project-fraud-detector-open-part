11be662a626ba3554cc670e7a10cea7adaf91f7f
Ildus Akhmadiev <anan308an@gmail.com>
2024-08-19 15:41:40 +0300

    first commit

--- src/main/java/org/javaguru/project_fraud_detector/task_1/frauddetector/Trader.java
+++ src/main/java/org/javaguru/project_fraud_detector/task_1/frauddetector/Trader.java
@@ -5,4 +5,9 @@ class Trader {
     private String fullName;
     private String city;
 
+    public Trader(String fullName, String city) {
+        this.fullName = fullName;
+        this.city = city;
+    }
+
 }
